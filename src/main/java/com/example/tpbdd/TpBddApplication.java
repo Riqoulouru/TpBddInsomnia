69950479178f08dffc9200dd098dd565cff7b660
Basile <basile.thiry@hotmail.fr>
2022-12-08 10:05:17 +0100

    first ocmmit

---
+++ src/main/java/com/example/tpbdd/TpBddApplication.java
@@ -0,0 +1,13 @@
+package com.example.tpbdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpBddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpBddApplication.class, args);
+    }
+
+}
