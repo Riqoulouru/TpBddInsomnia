bd6a86f574c8cd7cca4bd700885a51ce091340dc
Basile <basile.thiry@hotmail.fr>
2023-01-11 18:30:11 +0100

    change port

--- src/main/java/com/example/tpbdd/TpBddApplication.java
+++ src/main/java/com/example/tpbdd/TpBddApplication.java
@@ -3,11 +3,17 @@ package com.example.tpbdd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class TpBddApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TpBddApplication.class, args);
+        SpringApplication app = new SpringApplication(TpBddApplication.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "8083"));
+
+        app.run(args);
     }
 
 }
